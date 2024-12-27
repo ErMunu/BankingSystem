a1a1d2c82f799a9fe23c426de59309273af308b0
Master Muntazir <me@muntazir.in>
2024-12-27 22:21:04 +0530

    added colors and amount check

--- app.py
+++ app.py
@@ -1,93 +1,106 @@
-import time
-import getpass
-from datetime import date
 import function
+import getpass
+import time
+from datetime import date
+from colorama import init, Fore
 
-
-print("\nWelcome to the Console Banking System\n1. Create Account\n2. Login\n3. Exit\nEnter your choice: ")
+init()
+print(Fore.BLUE + "\nWelcome to the Console Banking System\n1. Create Account\n2. Login\n3. Exit\nEnter your choice: ")
 user_input = input();
 while user_input != '3':
     if user_input == '1':
         account_number = int(time.time() * 1_000_000) % (10**6)
         account_details = str(account_number)
         
-        print("\nEnter your name: ")
+        print(Fore.BLUE + "\nEnter your name: ")
         account_details += "," + input()
         
-        print("\nEnter your initial deposit: ")
-        deposit = input()
+        print(Fore.BLUE + "\nEnter your initial deposit: ")
+        try:
+            deposit = int(input())
+        except:
+            print(Fore.RED + "Invalid amount entered, please try again!")
+            continue
         
-        print(f"\nYour account number: {account_number} (Save this for login)")
+        print(Fore.GREEN + f"\nYour account number: {account_number} (Save this for login)")
         
-        password = function.custom_hash(getpass.getpass("Enter a password: "))
+        password = function.custom_hash(getpass.getpass(Fore.YELLOW + "Enter a password: "))
         account_details += f",{password},{deposit}"
         
         transaction_details = f"{account_number},Deposit,{deposit},{date.today().isoformat()}"
         
         if function.create_account(account_details):
-            print("\nAccount created successfully!")
+            print(Fore.GREEN + "\nAccount created successfully!")
             if function.add_transaction(transaction_details):
-                print(f"\nDeposit successful! Current balance: {deposit}")
+                print(Fore.GREEN + f"\nDeposit successful! Current balance: {deposit}")
             else:
-                print("\nDeposit Failed!")
+                print(Fore.RED + "\nDeposit Failed!")
         else:
-            print("\nFailed to create account. Please try again!")
+            print(Fore.RED + "\nFailed to create account. Please try again!")
                 
         
     elif user_input == '2':
-        print("\nEnter your account number: ")
+        print(Fore.BLUE + "\nEnter your account number: ")
         account_number = input()
-        password = function.custom_hash(getpass.getpass("Enter your password: "))
+        password = function.custom_hash(getpass.getpass(Fore.BLUE + "Enter your password: "))
         data_dict = function.check_credentials(account_number,password)
         if data_dict:
-            print("\nLogin successful!")
-            print(f"\n\nWelcome {data_dict[account_number][0]}!\n1. Deposit Money\n2. Withdraw Money\n3. Check Balance\n4. Logout\nEnter your choice: ")
+            print(Fore.GREEN + "\nLogin successful!")
+            print(Fore.BLUE + f"\n\nWelcome {data_dict[account_number][0]}!\n1. Deposit Money\n2. Withdraw Money\n3. Check Balance\n4. Logout\nEnter your choice: ")
             user_action = input()
             while user_action != '4':
                 #Deposit Money
                 if user_action == '1':
-                    print("\nEnter amount to deposit: ")
-                    deposit = input()
+                    print(Fore.BLUE + "\nEnter amount to deposit: ")
+                    try:
+                        deposit = int(input())
+                    except:
+                        print(Fore.RED + "Invalid amount entered, please try again!")
+                        continue
                     data_dict[account_number][2] = int(data_dict[account_number][2]) + int(deposit)
                     if function.dict_to_file(data_dict):
                         transaction_details = f"{account_number},Deposit,{deposit},{date.today().isoformat()}"
                         if function.add_transaction(transaction_details):
-                            print(f"\nDeposit successful! Current balance: {data_dict[account_number][2]}")
+                            print(Fore.GREEN + f"\nDeposit successful! Current balance: {data_dict[account_number][2]}")
                         else:
-                            print("\nDeposit failed, please try again!")
+                            print(Fore.RED + "\nDeposit failed, please try again!")
                     else:
-                        print("\nDeposit failed, please try again!")
+                        print(Fore.RED + "\nDeposit failed, please try again!")
                 #Withdraw Money
                 elif user_action == '2':
-                    print("\nEnter amount to withdraw: ")
-                    withdrawal = input()
+                    print(Fore.BLUE + "\nEnter amount to withdraw: ")
+                    try:
+                        withdrawal = int(input())
+                    except:
+                        print(Fore.RED + "Invalid amount entered, please try again!")
+                        continue
                     balance = int(data_dict[account_number][2]) - int(withdrawal)
                     if balance >= 0:
                         data_dict[account_number][2] = balance
                         if function.dict_to_file(data_dict):
                             transaction_details = f"{account_number},Withdrawal,{withdrawal},{date.today().isoformat()}"
                             if function.add_transaction(transaction_details):
-                                print(f"\nWithdrawal successful! Current balance: {data_dict[account_number][2]}")
+                                print(Fore.GREEN + f"\nWithdrawal successful! Current balance: {data_dict[account_number][2]}")
                             else:
-                                print("\nWithdrawal failed, please try again!")
+                                print(Fore.RED + "\nWithdrawal failed, please try again!")
                         else:
-                            print("\nWithdrawal failed, please try again!")
+                            print(Fore.RED + "\nWithdrawal failed, please try again!")
                     else:
-                        print("\nInsufficient balance.")
+                        print(Fore.RED + "\nInsufficient balance.")
                         
                 #Check Balance
                 elif user_action == '3':
-                    print(f"\nCurrent balance: {data_dict[account_number][2]}")
+                    print(Fore.GREEN + f"\nCurrent balance: {data_dict[account_number][2]}")
                 else:
-                    print("\nInvalid input. Please try again!\n")
+                    print(Fore.RED + "\nInvalid input. Please try again!\n")
                 
-                print(f"\n\nWelcome {data_dict[account_number][0]}!\n1. Deposit Money\n2. Withdraw Money\n3. Check Balance\n4. Logout\nEnter your choice: ")
+                print(Fore.BLUE + f"\n\nWelcome {data_dict[account_number][0]}!\n1. Deposit Money\n2. Withdraw Money\n3. Check Balance\n4. Logout\nEnter your choice: ")
                 user_action = input()
         else:
-            print("\nInvalid account number or password. Please try again!")
+            print(Fore.RED + "\nInvalid account number or password. Please try again!")
         
     else:
-        print("\nWrong option")
+        print(Fore.RED + "\nWrong option")
         
-    print("\nWelcome to the Console Banking System\n1. Create Account\n2. Login\n3. Exit\nEnter your choice: ")
+    print(Fore.BLUE + "\nWelcome to the Console Banking System\n1. Create Account\n2. Login\n3. Exit\nEnter your choice: ")
     user_input = input();
